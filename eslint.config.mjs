41368a188ae5f3541860eb7bea62b63c94606af3
Bruno Bornsztein <bruno.bornsztein@gmail.com>
2024-06-18 10:08:52 -0500

    ignore dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,34 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-const rules =  {
-  "@typescript-eslint/no-explicit-any": "warn",
-}
+
+
+
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  {rules}
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_"
+        }
+      ],
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off"
+
+    }
+  },
+  {
+    ignores: [
+      'dist/*'
+    ]
+  }
+
 ];
