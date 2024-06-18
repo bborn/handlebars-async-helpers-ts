4ce95f9468e457256559eef2699629893d612c60
Bruno Bornsztein <bruno.bornsztein@gmail.com>
2024-06-18 09:32:03 -0500

    fix

--- index.ts
+++ index.ts
@@ -1,82 +1,104 @@
-import app from './package.json';
-import { registerCoreHelpers } from './helpers';
+import app from "./package.json";
+import { registerCoreHelpers } from "./helpers";
 
-const isPromise = (obj: any): boolean => !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
+const isPromise = (obj: any): boolean =>
+  !!obj &&
+  (typeof obj === "object" || typeof obj === "function") &&
+  typeof obj.then === "function";
 
-function asyncHelpers(hbs: any) {
-
+export function asyncHelpers(hbs: any) {
   const handlebars = hbs.create(),
-        asyncCompiler = class extends hbs.JavaScriptCompiler {
+    asyncCompiler = class extends hbs.JavaScriptCompiler {
+      compiler: any;
 
-          compiler: any;
+      constructor() {
+        super();
+        this.compiler = asyncCompiler;
+      }
 
-          constructor() {
-            super();
-            this.compiler = asyncCompiler;
-          }
+      mergeSource(varDeclarations: any) {
+        const sources = super.mergeSource(varDeclarations);
+        sources.prepend("return (async () => {");
+        sources.add(" })()");
+        return sources;
+      }
 
-          mergeSource(varDeclarations: any) {
-            const sources = super.mergeSource(varDeclarations);
-            sources.prepend('return (async () => {');
-            sources.add(' })()');
-            return sources;
-          }
+      appendToBuffer(source: any, location: any, explicit: any) {
+        if (!Array.isArray(source)) {
+          source = [source];
+        }
+        source = this.source.wrap(source, location);
 
-          appendToBuffer(source: any, location: any, explicit: any) {
-            if (!Array.isArray(source)) {
-              source = [source];
-            }
-            source = this.source.wrap(source, location);
-
-            if (this.environment.isSimple) {
-              return ['return await ', source, ';'];
-            } if (explicit) {
-              return ['buffer += await ', source, ';'];
-            }
-            source.appendToBuffer = true;
-            source.prepend('await ');
-            return source;
-          }
-        };
+        if (this.environment.isSimple) {
+          return ["return await ", source, ";"];
+        }
+        if (explicit) {
+          return ["buffer += await ", source, ";"];
+        }
+        source.appendToBuffer = true;
+        source.prepend("await ");
+        return source;
+      }
+    };
 
   handlebars.JavaScriptCompiler = asyncCompiler;
 
   const _compile = handlebars.compile,
-        _template = handlebars.VM.template,
-        _escapeExpression = handlebars.escapeExpression,
-        escapeExpression = function(value: any) {
-          if(isPromise(value)) {
-            return value.then((v: any) => _escapeExpression(v));
-          }
-          return _escapeExpression(value);
-        };
+    _template = handlebars.VM.template,
+    _escapeExpression = handlebars.escapeExpression,
+    escapeExpression = function (value: any) {
+      if (isPromise(value)) {
+        return value.then((v: any) => _escapeExpression(v));
+      }
+      return _escapeExpression(value);
+    };
 
   function lookupProperty(containerLookupProperty: any) {
-    return function(parent: any, propertyName: any) {
+    return function (parent: any, propertyName: any) {
       if (isPromise(parent)) {
-        return parent.then((p: any) => containerLookupProperty(p, propertyName));
+        return parent.then((p: any) =>
+          containerLookupProperty(p, propertyName)
+        );
       }
       return containerLookupProperty(parent, propertyName);
     };
   }
 
-  handlebars.template = function(spec: any) {
-    spec.main_d = (prog: any, props: any, container: any, depth: any, data: any, blockParams: any, depths: any) => async(context: any) => {
-      container.escapeExpression = escapeExpression;
-      container.lookupProperty = lookupProperty(container.lookupProperty);
-      if(depths.length == 0){
-        depths = [data.root];
-      }
-      const v = spec.main(container, context, container.helpers, container.partials, data, blockParams, depths);
-      return v;
-    };
+  handlebars.template = function (spec: any) {
+    spec.main_d =
+      (
+        prog: any,
+        props: any,
+        container: any,
+        depth: any,
+        data: any,
+        blockParams: any,
+        depths: any
+      ) =>
+      async (context: any) => {
+        container.escapeExpression = escapeExpression;
+        container.lookupProperty = lookupProperty(container.lookupProperty);
+        if (depths.length == 0 && data) {
+          depths = [data.root];
+        }
+        const v = spec.main(
+          container,
+          context,
+          container.helpers,
+          container.partials,
+          data,
+          blockParams,
+          depths
+        );
+        return v;
+      };
     return _template(spec, handlebars);
   };
 
-  handlebars.compile = function(template: any, options: any) {
+  handlebars.compile = function (template: any, options: any) {
     const compiled = _compile.apply(handlebars, [template, { ...options }]);
 
-    return function(context: any, execOptions: any) {
+    return function (context: any, execOptions: any) {
       context = context || {};
 
       return compiled.call(handlebars, context, execOptions);
